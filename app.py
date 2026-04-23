3b9b216aedd4e65ba7a754e5ef8cd84433ca2d8e
Tonynopro <suarezperezricardo@gmail.com>
2026-04-22 23:03:57 -0600

    Lista prueba

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector
+from config import DB_CONFIG
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+
+def get_connection():
+    return mysql.connector.connect(**DB_CONFIG)
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "mensaje": "API funcionando"
+    })
+
+
+@app.route("/evento", methods=["POST"])
+def registrar_evento():
+
+    data = request.json
+
+    tipo = data.get("tipo")
+
+    if tipo not in ["entrada", "salida"]:
+        return jsonify({
+            "error": "Tipo inválido"
+        }), 400
+
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    query = """
+        INSERT INTO eventos (tipo)
+        VALUES (%s)
+    """
+
+    cursor.execute(query, (tipo,))
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+
+    return jsonify({
+        "mensaje": "Evento registrado"
+    })
+
+
+@app.route("/aforo", methods=["GET"])
+def obtener_aforo():
+
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT tipo, COUNT(*) 
+        FROM eventos
+        GROUP BY tipo
+    """)
+
+    resultados = cursor.fetchall()
+
+    entradas = 0
+    salidas = 0
+
+    for tipo, cantidad in resultados:
+
+        if tipo == "entrada":
+            entradas = cantidad
+
+        elif tipo == "salida":
+            salidas = cantidad
+
+    aforo = entradas - salidas
+
+    cursor.close()
+    connection.close()
+
+    return jsonify({
+        "aforo_actual": aforo
+    })
+
+
+if __name__ == "__main__":
+
+    PORT = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=PORT
+    )
